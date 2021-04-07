1e9db7a1213931ed0482d9edfad2b70135916439
Gabriel Escame <gabriel.escame@gmail.com>
2021-04-06 21:32:20 -0300

    feat: adding height and width to Icon component

--- src/components/foundation/Icon/index.tsx
+++ src/components/foundation/Icon/index.tsx
@@ -5,12 +5,15 @@ import IconMapper from '../../../assets/icons'
 interface IconInterface {
   iconId: string
   color?: string
+  height?: number
+  width?: number
 }
 
-const Icon: React.FC<IconInterface> = ({ iconId, color }) => {
+const Icon: React.FC<IconInterface> = ({ iconId, color, height, width }) => {
   const IconComponent = IconMapper[iconId]
+  console.log(height, width)
   return (
-    <IconContainer color={color}>
+    <IconContainer color={color} height={height} width={width}>
       <IconComponent />
     </IconContainer>
   )
--- src/components/foundation/Icon/style.ts
+++ src/components/foundation/Icon/style.ts
@@ -1,13 +1,20 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 interface IconContainerInterface {
   color: string
+  height?: number
+  width?: number
 }
 
 export const IconContainer = styled.div<IconContainerInterface>`
   display: flex;
   align-items: center;
-  color: ${({color = 'yellow', theme}) => theme.colors[color].main};
+  color: ${({ color = 'yellow', theme }) => theme.colors[color].main};
   width: fit-content;
   height: fit-content;
+
+  svg {
+    height: ${({ height = 48 }) => `${height}px`};
+    width: ${({ width = 48 }) => `${width}px`};
+  }
 `
